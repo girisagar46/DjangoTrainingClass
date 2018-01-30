b43f19d40142cdf507500b6114d17b3bffcfb716
sgiri <girisagar46@gmail.com>
2018-01-30 18:58:32 +0545

    Class Based View

--- Week_2/DjangoBlog/blog/urls.py
+++ Week_2/DjangoBlog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import post_list
+from .views import post_list, PostListView
 
 urlpatterns = [
-    url(r'^$', post_list, name='post_list'),
+    url(r'^$', PostListView.as_view(), name='post_list'),
+	#url(r'^$', post_list, name='post_list'),
 ]
--- Week_2/DjangoBlog/blog/views.py
+++ Week_2/DjangoBlog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Blog
+from django.views import View
 
 
 def post_list(request):
@@ -9,4 +10,8 @@ def post_list(request):
     }
     return render(request, "blog/post.html", context=ctx)
 
-
+class PostListView(View):
+	def get(self, request, *args, **kwargs):
+		blogs = Blog.objects.order_by('-created_date')
+		ctx = { "blogs": blogs }
+		return render(request, "blog/post.html", context=ctx)
