7b7c5206fcf9d68d682f4af0bc10f4add00fafad
girisagar46 <girisagar46@gmail.com>
2018-01-30 09:06:59 +0545

    Jan 30

--- Week_2/DjangoBlog/blog/views.py
+++ Week_2/DjangoBlog/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 def post_list(request):
-    return render(request, "blog/post.html", context={})
+    blogs = Blog.objects.order_by('-created_date')
+    ctx = {
+        "blogs": blogs
+    }
+    return render(request, "blog/post.html", context=ctx)
 
 
