3cde1e893173b9facf437c9c3136d8afafaa0a21
girisagar46 <girisagar46@gmail.com>
2018-01-31 08:37:05 +0545

    Jan 31

--- Week_2/DjangoBlog/blog/urls.py
+++ Week_2/DjangoBlog/blog/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from .views import post_list, PostListView
+from .views import ContactView, PostDetailView, PostListView
+
 
 urlpatterns = [
-    url(r'^$', PostListView.as_view(), name='post_list'),
-	#url(r'^$', post_list, name='post_list'),
+    # url(r'^$', post_list, name='post_list'),
+    url(r'^$', PostListView.as_view(), name='post_view'),
+    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail'),
+    url(r'^about/', TemplateView.as_view(template_name='blog/about.html'), name='about'),
+    url(r'^contact/', ContactView.as_view(), name='contact'),
 ]
--- Week_2/DjangoBlog/blog/views.py
+++ Week_2/DjangoBlog/blog/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+
 from .models import Blog
-from django.views import View
 
 
-def post_list(request):
-    blogs = Blog.objects.order_by('-created_date')
-    ctx = {
-        "blogs": blogs
-    }
-    return render(request, "blog/post.html", context=ctx)
+class PostListView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        blogs = Blog.objects.order_by('-created_date')
+        ctx = {"blogs": blogs}
+        return render(request, "blog/post.html", context=ctx)
 
-class PostListView(View):
-	def get(self, request, *args, **kwargs):
-		blogs = Blog.objects.order_by('-created_date')
-		ctx = { "blogs": blogs }
-		return render(request, "blog/post.html", context=ctx)
+
+class ContactView(TemplateView):
+    template_name = 'blog/contacts.html'
+
+
+class PostDetailView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(Blog, pk=kwargs.get("pk"))
+        ctx = { "post": post }
+        return render(request, "blog/post_detail.html", context=ctx)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# class PostView(TemplateView):
+#     template_name = "blog/post.html"
+#
+#     def get_context_data(self, *args, **kwargs):
+#         context = super(PostView, self).get_context_data(*args, **kwargs)
+#         print(context)
+#         blogs = Blog.objects.order_by('-created_date')
+#         context = {"blogs": blogs}
+#
+#         return context
